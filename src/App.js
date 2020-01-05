c36082aa0f3d4a2511c967dbe78574f05dc2ceb4
saymecode <fedor.filippov@rexsoftinc.com>
2020-01-05 17:45:41 +0200

    mobX implementation and test todo scripts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,57 @@ import React from 'react';
 import './App.css';
 import { observer } from 'mobx-react'
 
-const App = observer((props) => (<h1>{props.store.todos[0]}</h1>))
+export default @observer class App extends React.Component{
+  filter(e) {
+    this.props.store.filter = e.target.value
+  }
 
-export default App
+  createNewTodo(e) {
+    if(e.which === 13) {
+      this.props.store.createNewTodo(e.target.value)
+      e.target.value = ''
+    }
+  }
+
+  toggleChecked(todo) {
+    todo.checked = !todo.checked
+  }
+
+  render () {
+    const { filter, filtredTodos } = this.props.store
+    const todosList = filtredTodos.map((todo)=>{
+      let parts = filter ? todo.value.split(new RegExp(`(${filter})`, 'gi')) : [todo.value]
+
+      return <li key={todo.id}>
+        <input type="checkbox" value={todo.checked} onChange={this.toggleChecked.bind(this, todo)} checked={todo.checked}/>
+        { parts.map(part => filter && part.toLowerCase() === filter.toLowerCase() ? <b key={todo.id + Math.random()}>{part}</b> : part)}
+      </li>;
+    })
+
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <div>
+          <div>
+            <label htmlFor="create">Create Task (Press Enter):</label>
+          </div>
+          <div>
+            <input name='create' className='create' onKeyPress={this.createNewTodo.bind(this)}/>
+          </div>
+        </div>
+        <br/>
+        <div>
+          <div>
+            <label htmlFor="filter">Filter:</label>
+          </div>
+          <div>
+            <input name='filter' className='filter' value={filter} onChange={this.filter.bind(this)}/>
+          </div>
+        </div>
+        <ul>
+          {todosList}
+        </ul>
+      </div>
+    )
+  }
+}
--- src/AppStorage.js
+++ src/AppStorage.js
@@ -1,8 +1,32 @@
-import { autorun, observable } from 'mobx'
+import { computed, observable } from 'mobx'
 
-class TodoStore {
-  @observable todos = []
-  @observable filter = ''
+class Task {
+  @observable value
+  @observable id
+  @observable checked
+
+  constructor (value) {
+    this.value = value
+    this.id = Date.now() + Math.random()
+    this.checked = false
+  }
 }
 
-export default TodoStore
+class TodoStore {
+  @observable todos = [new Task('Learn MobX'), new Task('Learn React')]
+  @observable filter = ''
+  @computed get filtredTodos() {
+    return this.todos.filter((todo) => {
+      if(!this.filter) {
+        return true
+      }
+
+      return todo.value.match(new RegExp(this.filter, 'gi'))
+    })
+  }
+  createNewTodo(value) {
+    this.todos.push(new Task(value))
+  }
+}
+
+export default new TodoStore()
